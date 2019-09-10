2124dcc98b37c1b19835e459524a31c85508a9ac
pronoy <beingpronoy@gmail.com>
2019-09-10 23:06:16 +0530

    separate pages for sign up and favicon change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { SignupComponent } from './signup/signup.component';
 import { BannerComponent } from './banner/banner.component';
 import { SigninComponent } from './signin/signin.component';
+import { StudentsignupComponent } from './studentsignup/studentsignup.component';
+import { MentorsignupComponent } from './mentorsignup/mentorsignup.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full'},
-  { path: 'signup', component: SignupComponent },
   { path: 'home', component: BannerComponent},
-  { path : 'signin', component: SigninComponent}
+  { path : 'signin', component: SigninComponent},
+  { path: 'studentsignup', component : StudentsignupComponent},
+  { path: 'mentorsignup', component : MentorsignupComponent}
 ];
 
 @NgModule({
---
+++ src/app/mentorsignup/mentorsignup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mentorsignup',
+  templateUrl: './mentorsignup.component.html',
+  styleUrls: ['./mentorsignup.component.scss']
+})
+export class MentorsignupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

a3df459605cf80b1772f47ad887533c692c28d1a
Pronoy Sarkar <pronoysarkar16@gmail.com>
2019-09-14 17:18:39 +0530

    partial mentor form validation added

---
+++ src/app/mentor-model.spec.ts
@@ -0,0 +1,7 @@
+import { MentorModel } from './mentor-model';
+
+describe('MentorModel', () => {
+  it('should create an instance', () => {
+    expect(new MentorModel()).toBeTruthy();
+  });
+});
--- src/app/mentorsignup/mentorsignup.component.ts
+++ src/app/mentorsignup/mentorsignup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+// import { MentorModel } from '../mentor-model';
 
 @Component({
   selector: 'app-mentorsignup',
@@ -8,6 +9,11 @@ import { Component, OnInit } from '@angular/core';
 export class MentorsignupComponent implements OnInit {
 
   constructor() { }
+  public techs = ['Java','C++', 'Python', 'Javascript', 'Android', 'Full Stack Development'
+                  ,'AngularJS', 'ReactJS','.Net','Ruby','Ios', 'Machine Learning',
+                'Deep Learning', 'Cloud Technology', 'IOT', 'DevOps', 'Business Management']
+
+  // mentorModel = new MentorModel('Pronoy', 'p@g.com', 9876543210, 3, "11-08-2018", "14-10-2018",   )
 
   ngOnInit() {
   }

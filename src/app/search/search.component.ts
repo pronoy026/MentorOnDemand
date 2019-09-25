bc7b5eb4abe5cf39ec7f1361020bc4887096f71d
pronoy <beingpronoy@gmail.com>
2019-09-25 09:18:38 +0530

    advanced search

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -9,6 +9,12 @@ export class SearchComponent implements OnInit {
 
   constructor() { }
 
+  public techs = ['Java', 'C++', 'Python', 'Javascript', 'Android', 'Full Stack Development'
+    , 'AngularJS', 'ReactJS', '.Net', 'Ruby', 'Ios', 'Machine Learning',
+    'Deep Learning', 'Cloud Technology', 'IOT', 'DevOps', 'Business Management']
+
+  public durations = ['1 week', '2 weeks', '3 weeks', '1 month', '2 month','3 months','6 months', '1 year']
+
   ngOnInit() {
   }
 
